e977fe72ec448d56d37f351221085003009769c7
TobiasPleyer <tobi.pleyer@gmail.com>
2019-04-28 23:06:10 +0200

    Script to create a new blog post blueprint

---
+++ new-post.hs
@@ -0,0 +1,43 @@
+#!/usr/bin/env stack
+{- stack
+   script
+   --resolver=lts-13.8
+   --package base
+   --package text
+   --package time
+-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Time.Clock
+import           Data.Time.Calendar
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import           System.Environment (getArgs)
+
+
+type Y = Integer
+type M = Int
+type D = Int
+
+
+getDate :: IO (Y,M,D)
+getDate = (toGregorian . utctDay) <$> getCurrentTime
+
+
+mkDateString :: Y -> M -> D -> String
+mkDateString y m d = year ++ "-" ++ month ++ "-" ++ day
+  where
+    year = show y
+    month = if m > 9 then show m else "0" ++ show m
+    day = if d > 9 then show d else "0" ++ show d
+
+
+main = do
+  title <- (T.pack . head) <$> getArgs
+  tmpl <- TIO.readFile "templates/new-post.markdown"
+  (y,m,d) <- getDate
+  let date = T.pack $ mkDateString y m d
+      content = T.replace "date: X" ("date: " <> date) $
+                T.replace "title: X" ("title: " <> title) tmpl
+      filepath = T.unpack $ "posts/" <> date <> "-" <> title <> ".markdown"
+  TIO.writeFile filepath content
---
+++ templates/new-post.markdown
@@ -0,0 +1,24 @@
+---
+title: X
+date: X
+tags: 
+category: Programming
+authors: Tobias Pleyer
+summary: 
+---
+
+Title
+=====
+
+Text
+
+Subtitle
+--------
+
+[link](www.example.com)
+
+```haskell
+code
+```
+::: {.code-include lexer="haskell" file="code/file.hs"}
+:::
