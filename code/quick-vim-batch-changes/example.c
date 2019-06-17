962aaabfbd72c374f0008e82c3ee9ddde13e591a
TobiasPleyer <tobi.pleyer@gmail.com>
2019-06-17 23:21:18 +0200

    Slight change to example.c

--- code/quick-vim-batch-changes/example.c
+++ code/quick-vim-batch-changes/example.c
@@ -5,19 +5,18 @@ int y = -1;
 
 enum WeekDay
 {
-    Mon = 1,
-    Tue = 2,
-    Wed = 3,
-    Thu = 4,
-    Fri = 5,
-    Sat = 6,
-    Sun = 7
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
 };
-
-const char* quantifiers[] = {"st", "nd", "rd", "th"};
 
 const char* get_quantifier(unsigned int num)
 {
+    static const char* quantifiers[] = {"st", "nd", "rd", "th"};
     switch (num)
     {
         case 1:
@@ -31,15 +30,8 @@ const char* get_quantifier(unsigned int num)
     }
 }
 
-enum WeekDay mon = Mon;
-enum WeekDay tue = Tue;
-enum WeekDay wed = Wed;
-enum WeekDay thu = Thu;
-
 int main()
 {
-    printf("Monday is the %i%s day of the week\n", (int)mon, get_quantifier(mon));
-    printf("Tuesday is the %i%s day of the week\n", (int)tue, get_quantifier(tue));
-    printf("Wednesday is the %i%s day of the week\n", (int)wed, get_quantifier(wed));
-    printf("Thursday is the %i%s day of the week\n", (int)thu, get_quantifier(thu));
+    enum WeekDay sunday = Sunday;
+    printf("Sunday is the %i%s day of the week\n", (int)sunday, get_quantifier(sunday));
 }
